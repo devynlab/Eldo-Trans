57eb82d6594626af558e88af35037b35779afdc3
Kosgey Laban <devynlab@gmail.com>
2022-06-02 19:59:14 +0300

    add 'fromTrip' method to TripDTO

--- src/main/java/io/devynlab/eldotrans/system/trip/dto/TripDTO.java
+++ src/main/java/io/devynlab/eldotrans/system/trip/dto/TripDTO.java
@@ -1,6 +1,7 @@
 package io.devynlab.eldotrans.system.trip.dto;
 
 import io.devynlab.eldotrans.system.booking.enums.Destinations;
+import io.devynlab.eldotrans.system.trip.model.Trip;
 import io.devynlab.eldotrans.system.vehicle.model.Vehicle;
 import lombok.Data;
 
@@ -17,4 +18,16 @@ public class TripDTO implements Serializable {
   private Integer numOfPassengers;
   private Destinations tripFrom;
   private Destinations tripTo;
+
+  public TripDTO fromTrip(Trip trip) {
+    id = trip.getId();
+    date = trip.getDate();
+    departedAt = trip.getDepartedAt();
+    arrivedAt = trip.getArrivedAt();
+    vehicle = trip.getVehicle();
+    numOfPassengers = trip.getNumOfPassengers();
+    tripFrom = trip.getTripFrom();
+    tripTo = trip.getTripTo();
+    return this;
+  }
 }
